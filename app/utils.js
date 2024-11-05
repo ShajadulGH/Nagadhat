c8ee373042fd0a7f850f5cbe6f7d5488cbe635d4
MD Naem <121675549+naem0@users.noreply.github.com>
2024-11-05 11:26:22 +0600

    static sit genaration off

--- app/utils.js
+++ app/utils.js
@@ -1,8 +1,6 @@
 // For Live
 // export const apiBaseUrl = "https://v3.nagadhat.com/api";
 // export const NagadhatPublicUrl = "https://v3.nagadhat.com";
-// export const VercerUrl = "https://nagadhat-dev.vercel.app";
-export const frontendLocalUrl = "http://localhost:3000";
 
 // For Live staging way
 export const apiBaseUrl = "https://v3.staging.nagadhat.com/api";
