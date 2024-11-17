9faae3c01786e721d37b8fa608c097b3d2aa8efb
MD Naem <121675549+naem0@users.noreply.github.com>
2024-11-17 18:52:44 +0600

    logout url add

--- app/components/SignoutBtn.jsx
+++ app/components/SignoutBtn.jsx
@@ -1,18 +1,15 @@
 "use client";
 import Link from "next/link";
 import { signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import { FaSignOutAlt } from "react-icons/fa";
 
 const SignoutBtn = () => {
-    const router = useRouter();
     return (
         <Link
             className="text-capitalize d-flex align-items-center"
             onClick={(e) => {
                 e.preventDefault();
-                signOut();
-                router.push("/");
+                signOut({ callbackUrl: '/' });
             }}
             href={`#`}
             style={{ padding: "10px 28px" }}
