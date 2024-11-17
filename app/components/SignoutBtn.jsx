9c4e1edc7cae34e3efc847abc2b648fb59882d27
MD Naem <121675549+naem0@users.noreply.github.com>
2024-11-17 19:36:04 +0600

    logout url add

--- app/components/SignoutBtn.jsx
+++ app/components/SignoutBtn.jsx
@@ -5,18 +5,17 @@ import { FaSignOutAlt } from "react-icons/fa";
 
 const SignoutBtn = () => {
     return (
-        <Link
-            className="text-capitalize d-flex align-items-center"
+        <div
+            className="text-capitalize d-flex align-items-center cars"
             onClick={(e) => {
                 e.preventDefault();
                 signOut({ callbackUrl: '/' });
             }}
-            href={`#`}
             style={{ padding: "10px 28px" }}
         >
             <FaSignOutAlt className="nav-icon me-2" />
             Logout
-        </Link>
+        </div>
     );
 };
 
