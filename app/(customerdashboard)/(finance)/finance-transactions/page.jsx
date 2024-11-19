08f731ddce7155dfa60e634da9ad9893484e8c7a
MD Naem <121675549+naem0@users.noreply.github.com>
2024-11-19 12:28:56 +0600

    finance pagination and search

--- app/(customerdashboard)/(finance)/finance-transactions/page.jsx
+++ app/(customerdashboard)/(finance)/finance-transactions/page.jsx
@@ -8,12 +8,13 @@ const FinanceTransactions = async ({ searchParams }) => {
     // get server session
     const session = await getServerSession(authOptions);
     const currentPage = parseInt(searchParams.page) || 1;
-    const limit = 4; //Per Page Category
+    const limit = 20; //Per Page Category
     
     // fetch affiliate finance transfer history data
     const data = await geAffiliateFinanceTransitions(session?.accessToken, currentPage, limit);
     const lastPage = data?.results?.last_page || 1;
     const transactions = data?.results?.data || [];
+    const serialNumber = (currentPage - 1 ) * limit;
 
     return (
         <div className="customer-dashboard-order-history-area">
@@ -36,7 +37,7 @@ const FinanceTransactions = async ({ searchParams }) => {
                         {transactions.length > 0 ? (
                             transactions.map((transaction, index) => (
                                 <tr key={transaction.id}>
-                                    <td>{index + 1}</td>
+                                    <td>{index + 1 + serialNumber}</td>
                                     <td>{transaction.date_time}</td>
                                     <td>{transaction.purpose}</td>
                                     <td className="text-end">{transaction.debit ? transaction.debit : "--"}</td>
