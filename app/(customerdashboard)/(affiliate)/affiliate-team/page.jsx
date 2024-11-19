bb936e0bdb7f9d8445680824472d7a814310b8f1
MD Naem <121675549+naem0@users.noreply.github.com>
2024-11-19 17:11:11 +0600

    dropdown nav activ

---
+++ app/(customerdashboard)/(affiliate)/affiliate-team/page.jsx
@@ -0,0 +1,11 @@
+import AffiliateTeamWrapp from "@/app/components/customerDashboard/affiliate/affiliatemyteam/AffiliateTeamWrapp";
+
+const AffiliateTeamPage = () => {
+    return (
+        <>
+            <AffiliateTeamWrapp />
+        </>
+    );
+};
+
+export default AffiliateTeamPage;
