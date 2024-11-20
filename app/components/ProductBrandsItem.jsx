d6316620884b050a48fe8bff1e032631f0b6472b
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-11-20 18:23:20 +0600

    container design chnage

--- app/components/ProductBrandsItem.jsx
+++ app/components/ProductBrandsItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { NagadhatPublicUrl, truncateTitle } from "../utils";
-import img from "@/public/images/placeholder--image.jpg"
+import img from "@/public/images/placeholder--image.jpg";
 
 const ProductBrandsItem = ({ categoryItem }) => {
     let imageurl = `${NagadhatPublicUrl}/${categoryItem.logo}`;
@@ -17,21 +17,22 @@ const ProductBrandsItem = ({ categoryItem }) => {
     return (
         <div className="nh-categories-item">
             <div className="nh-categories-item-bg nh-hover-box-shadow">
-                <Link href={`category/${path}`}>
-                    <div className="nh-categories-img image-hover-effect">
-                        <Image
-                            width={150} height={150}
-                            src={imageurl?imageurl:img}
-                            className="img-fluid"
-                            alt={altText}
-                            // fill={true}
-                            unoptimized
-                        />
-                    </div>
-                    <div className="nh-categories-info text-hover-effect text-capitalize text-center">
-                        <h4>{truncateTitle(title, 20)}</h4>
-                    </div>
-                </Link>
+                {/* <Link href={`#`}> */}
+                <div className="nh-categories-img image-hover-effect">
+                    <Image
+                        width={150}
+                        height={150}
+                        src={imageurl ? imageurl : img}
+                        className="img-fluid"
+                        alt={altText}
+                        // fill={true}
+                        unoptimized
+                    />
+                </div>
+                <div className="nh-categories-info text-hover-effect text-capitalize text-center">
+                    <h4>{truncateTitle(title, 20)}</h4>
+                </div>
+                {/* </Link> */}
             </div>
         </div>
     );
