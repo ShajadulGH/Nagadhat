2ac07f535a7fdb82dd0cdea8f4d7c679e9513edb
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-11-26 19:32:43 +0600

    support page create

---
+++ app/(customerdashboard)/(support)/add-customer-support-ticket/page.jsx
@@ -0,0 +1,11 @@
+const AddCustomerSupportTicketPage = () => {
+    return (
+        <div className="h-100 customer-dashboard-card">
+            <div className="px-4 py-3">
+                <span>Create Ticket</span>
+            </div>
+        </div>
+    );
+};
+
+export default AddCustomerSupportTicketPage;
