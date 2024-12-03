13cd6a8a048e05b7ace7d88661a490cfafa3ffb1
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-12-03 13:12:16 +0600

    brand and category api remove tags in next js

--- app/services/getHomeFlashSalesProduct.js
+++ app/services/getHomeFlashSalesProduct.js
@@ -9,8 +9,7 @@ export const getHomeFlashSalesProduct = async (
         try {
             const response = await fetch(
                 `${apiBaseUrl}/get-outlet-product-by-district-id-v2-flash-sale/${districtId}?page=${page}&limit=${limit}`,
-                ["posts"],
-                { revalidate: 1, tags: ["posts"] }
+                { next: { revalidate: 1 } }
             );
             return await response.json();
         } catch (error) {
