4334a049de04a1cd856e99acdef57b579e3c1ca3
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-12-12 19:15:39 +0600

    affiliate-pending-balance API impliment

--- app/components/customerDashboard/payout/PendingBalanceLists.jsx
+++ app/components/customerDashboard/payout/PendingBalanceLists.jsx
@@ -3,10 +3,10 @@ const PendingBalanceLists = ({ item, index }) => {
         <tr>
             <td scope="row">{index + 1}</td>
             <td>{item?.date_time || "N/A"}</td>
-            <td>Particular</td>
-            <td>{item?.received || "N/A"}</td>
+            <td>{item?.particular}</td>
+            <td>{item?.from || "N/A"}</td>
             <td>{item?.purpose || "N/A"}</td>
-            <td className="text-end">৳ {item?.amount.toFixed(2) || "0.00"}</td>
+            <td className="text-end">৳ {item?.amount || "0.00"}</td>
         </tr>
     );
 };
