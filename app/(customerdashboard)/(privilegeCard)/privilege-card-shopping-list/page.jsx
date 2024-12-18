d41533dfd8be2ecc72a55eaa921aad07f3b70944
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-12-18 19:35:34 +0600

    cancel status in privilege card page

--- app/(customerdashboard)/(privilegeCard)/privilege-card-shopping-list/page.jsx
+++ app/(customerdashboard)/(privilegeCard)/privilege-card-shopping-list/page.jsx
@@ -2,11 +2,32 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import PrivilegeCardShoppingWrapper from "@/app/components/customerDashboard/privilegeDashboard/PrivilegeCardShoppingWrapper";
 import { getPrivilegeCardProducts } from "@/app/services/privilegeCard/getPrivilegeCardProducts";
 import { getServerSession } from "next-auth";
+import { useSession } from "next-auth/react";
 
 const PrivilegeCardShoppingListPage = async () => {
-    const session = await getServerSession(authOptions);
-    const response = await getPrivilegeCardProducts(session?.accessToken);
-    const perCardLimit = response?.results?.card_limit;
+    // const session = await getServerSession(authOptions);
+    // const response = await getPrivilegeCardProducts(session?.accessToken);
+    // const perCardLimit = response?.results?.card_limit;
+
+    const { data: session } = useSession();
+    const [perCardLimit, setPerCardLimit] = useState(undefined);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (session?.accessToken) {
+                try {
+                    const response = await getPrivilegeCardProducts(
+                        session?.accessToken
+                    );
+                    setPerCardLimit(response?.results?.card_limit);
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+        };
+
+        fetchData();
+    }, [session?.accessToken]);
 
     return (
         <>
