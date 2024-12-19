1e94a0947334f6c0fe1f1f5b043d8777ceb0db3c
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-12-19 10:51:25 +0600

    server to client

--- app/(customerdashboard)/(privilegeCard)/privilege-card-shopping-list/page.jsx
+++ app/(customerdashboard)/(privilegeCard)/privilege-card-shopping-list/page.jsx
@@ -1,14 +1,11 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+"use client";
+
 import PrivilegeCardShoppingWrapper from "@/app/components/customerDashboard/privilegeDashboard/PrivilegeCardShoppingWrapper";
 import { getPrivilegeCardProducts } from "@/app/services/privilegeCard/getPrivilegeCardProducts";
-import { getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const PrivilegeCardShoppingListPage = async () => {
-    // const session = await getServerSession(authOptions);
-    // const response = await getPrivilegeCardProducts(session?.accessToken);
-    // const perCardLimit = response?.results?.card_limit;
-
     const { data: session } = useSession();
     const [perCardLimit, setPerCardLimit] = useState(undefined);
 
