4c8a973fad475771ca2e12d056a21b5dc189e59c
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-12-19 11:55:16 +0600

    transaction page added

--- app/components/customerDashboard/settings/passwordtxnotp/TransactionOtpChoiceModal.jsx
+++ app/components/customerDashboard/settings/passwordtxnotp/TransactionOtpChoiceModal.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-const TransactionOtpChoiceModal = ({ handleManageOtpChange, modalRef, mobileNumber }) => {
+const TransactionOtpChoiceModal = ({
+    handleManageOtpChange,
+    modalRef,
+    mobileNumber,
+}) => {
     return (
         <>
             <div
@@ -20,15 +20,32 @@ const TransactionOtpChoiceModal = ({ handleManageOtpChange, modalRef, mobileNumb
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="TransactionOtpLabel">
+                            <h1
+                                className="modal-title fs-5"
+                                id="TransactionOtpLabel"
+                            >
                                 Send OTP To
                             </h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
                         </div>
                         <div className="modal-body">
                             <div className="form-check">
-                                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked/>
-                                <label className="form-check-label" for="flexRadioDefault1">
+                                <input
+                                    className="form-check-input"
+                                    type="radio"
+                                    name="flexRadioDefault"
+                                    id="flexRadioDefault1"
+                                    checked
+                                />
+                                <label
+                                    className="form-check-label"
+                                    for="flexRadioDefault1"
+                                >
                                     Phone - {mobileNumber}
                                 </label>
                             </div>
@@ -38,102 +55,27 @@ const TransactionOtpChoiceModal = ({ handleManageOtpChange, modalRef, mobileNumb
                                     Email - 9876543210@example.com
                                 </label>
                             </div> */}
-                            <p><small>* choose from your phone or email for OTP code.</small></p>
+                            <p>
+                                <small>
+                                    * choose from your phone or email for OTP
+                                    code.
+                                </small>
+                            </p>
                         </div>
                         <div className="modal-footer">
-                            <button onClick={handleManageOtpChange} type="button" className="add-to-cart-link border-0 w-100">Sand</button>
-=======
-const TransactionOtpChoiceModal = () => {
-=======
-const TransactionOtpChoiceModal = ({handleManageOtpChange, modalRef}) => {
->>>>>>> e134429 (updated)
-=======
-const TransactionOtpChoiceModal = ({ handleManageOtpChange, modalRef, mobileNumber }) => {
->>>>>>> c642eee (Manage Pin or Otp)
-    return (
-        <>
-            <div
-                ref={modalRef}
-                className="modal fade"
-                id="TransactionOtp"
-                data-bs-backdrop="static"
-                data-bs-keyboard="false"
-                tabindex="-1"
-                aria-labelledby="TransactionOtpLabel"
-                aria-hidden="true"
-            >
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="TransactionOtpLabel">
-                                Send OTP To
-                            </h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <div className="form-check">
-                                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked/>
-                                <label className="form-check-label" for="flexRadioDefault1">
-                                    Phone - {mobileNumber}
-                                </label>
-                            </div>
-                            {/* <div className="form-check">
-                                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                                <label className="form-check-label" for="flexRadioDefault2">
-                                    Email - 9876543210@example.com
-                                </label>
-                            </div> */}
-                            <p><small>* choose from your phone or email for OTP code.</small></p>
-                        </div>
-<<<<<<< HEAD
-                        <div class="modal-footer justify-content-between">
-                            {/* <button type="button" class="add-to-cart-link border-0 bg-dark-subtle" data-bs-dismiss="modal">Close</button> */}
-<<<<<<< HEAD
-                            <button type="button" class="add-to-cart-link border-0 w-100">Sand</button>
->>>>>>> 88913d0 (updated)
-=======
-                            <button onClick={handleManageOtpChange} type="button" class="add-to-cart-link border-0 w-100">Sand</button>
->>>>>>> e134429 (updated)
-=======
-                        <div className="modal-footer">
-                            <button onClick={handleManageOtpChange} type="button" className="add-to-cart-link border-0 w-100">Sand</button>
->>>>>>> c642eee (Manage Pin or Otp)
-=======
-const TransactionOtpChoiceModal = () => {
-    return (
-        <>
-            <div class="modal fade" id="TransactionOtp" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="TransactionOtpLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="TransactionOtpLabel">Send OTP To                            </h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
-                                    <label class="form-check-label" for="flexRadioDefault1">
-                                        Phone - 01310881055
-                                    </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"/>
-                                    <label class="form-check-label" for="flexRadioDefault2">
-                                        Email - 9876543210@example.com
-                                    </label>
-                            </div>
-                            <p><small>* choose from your phone or email for OTP code.</small></p>
-                        </div>
-                        <div class="modal-footer justify-content-between">
-                            {/* <button type="button" class="add-to-cart-link border-0 bg-dark-subtle" data-bs-dismiss="modal">Close</button> */}
-                            <button type="button" class="add-to-cart-link border-0 w-100">Sand</button>
->>>>>>> 7764139 (updated)
+                            <button
+                                onClick={handleManageOtpChange}
+                                type="button"
+                                className="add-to-cart-link border-0 w-100"
+                            >
+                                Sand
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default TransactionOtpChoiceModal
+export default TransactionOtpChoiceModal;
