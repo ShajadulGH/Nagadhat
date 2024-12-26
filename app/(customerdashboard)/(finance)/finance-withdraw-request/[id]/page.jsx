df01a7fb1448d7d6053d3fd467b6880f48b29c4a
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-12-26 13:46:19 +0600

    some issue fixed

--- app/(customerdashboard)/(finance)/finance-withdraw-request/[id]/page.jsx
+++ app/(customerdashboard)/(finance)/finance-withdraw-request/[id]/page.jsx
@@ -13,8 +13,6 @@ const FinanceWithdraw = ({ params }) => {
     const { data: session } = useSession();
     const { id } = params;
     const route = useRouter();
-    console.log("Withdraw Request Data:", withdrawRequestData);
-    
     // Fetch withdraw details
     useEffect(() => {
         const fetchWithdrawDetails = async () => {
