204e87aa1b9c78f78f9eda2e3aeaf61b8fa00c57
MD Naem <121675549+naem0@users.noreply.github.com>
2025-01-01 16:55:39 +0600

    Refactor MyTeamList to calculate total amounts correctly and add debug logging for displayMembers

--- app/components/customerDashboard/affiliate/affiliatemyteam/MyTeamList.jsx
+++ app/components/customerDashboard/affiliate/affiliatemyteam/MyTeamList.jsx
@@ -16,6 +16,8 @@ const MyTeamList = ({ teamListInfo, teamGrandTotal }) => {
     );
     const displayMembers = [...generalMembers, ...affiliateMembers];
 
+    console.log("displayMembers", displayMembers);
+
     return (
         <div className="table-responsive">
             <table className="table table-hover" style={{ minWidth: "950px" }}>
@@ -57,13 +59,13 @@ const MyTeamList = ({ teamListInfo, teamGrandTotal }) => {
                             </td>
                             <td className="text-end">
                                 ৳{" "}
-                                {member.affiliate_user?.total_resell_amount.toFixed(
+                                {(member.affiliate_user?.team_total_resell_amount + member?.affiliate_user?.team_total_container_amount ).toFixed(
                                     2
                                 ) || 0}
                             </td>
                             <td className="text-end">
                                 ৳{" "}
-                                {member.affiliate_user?.total_retail_amount.toFixed(
+                                {member.affiliate_user?.team_total_retail_amount.toFixed(
                                     2
                                 ) || 0}
                             </td>
@@ -93,9 +95,7 @@ const MyTeamList = ({ teamListInfo, teamGrandTotal }) => {
                             <strong>
                                 Total ৳{" "}
                                 {teamGrandTotal?.grand_total_resell_amount
-                                    ? teamGrandTotal?.grand_total_resell_amount.toFixed(
-                                          2
-                                      )
+                                    ? teamGrandTotal?.grand_total_resell_amount.toFixed(2)
                                     : 0}
                             </strong>
                         </td>
