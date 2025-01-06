1acfdccf2a8faf81c1c89ae4f55fbf23922b04c6
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-01-06 11:43:14 +0600

    Refactor MyTeamList and PrivilegeAddToCard for improved readability and formatting

--- app/components/customerDashboard/affiliate/affiliatemyteam/MyTeamList.jsx
+++ app/components/customerDashboard/affiliate/affiliatemyteam/MyTeamList.jsx
@@ -60,8 +60,8 @@ const MyTeamList = ({ teamListInfo, teamGrandTotal }) => {
                             <td className="text-end">
                                 ৳{" "}
                                 {(
-                                    parseInt(member?.affiliate_user?.team_total_resell_amount || 0) +
-                                    parseInt(member?.affiliate_user?.team_total_container_amount || 0)
+                                    parseInt(member.affiliate_user?.team_total_resell_amount || 0) +
+                                    parseInt(member.affiliate_user?.team_total_container_amount || 0)
                                 ).toLocaleString()}
                             </td>
                             <td className="text-end">
@@ -95,7 +95,9 @@ const MyTeamList = ({ teamListInfo, teamGrandTotal }) => {
                             <strong>
                                 Total ৳{" "}
                                 {teamGrandTotal?.grand_total_resell_amount
-                                    ? teamGrandTotal?.grand_total_resell_amount.toFixed(2)
+                                    ? teamGrandTotal?.grand_total_resell_amount.toFixed(
+                                          2
+                                      )
                                     : 0}
                             </strong>
                         </td>
@@ -103,7 +105,9 @@ const MyTeamList = ({ teamListInfo, teamGrandTotal }) => {
                             <strong>
                                 Total ৳{" "}
                                 {teamGrandTotal?.grand_total_retail_amount
-                                    ? teamGrandTotal?.grand_total_retail_amount.toFixed(2)
+                                    ? teamGrandTotal?.grand_total_retail_amount.toFixed(
+                                        2
+                                    )
                                     : 0}
                             </strong>
                         </td>
