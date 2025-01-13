3888fc28be5fe30d3f8d3ae376647c421eb6f792
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-01-13 14:18:25 +0600

    Remove commented-out phone number validation from login and registration pages

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -45,11 +45,11 @@ const Login = () => {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        const allowedNumbers = ["01739245723", "01680572792", "01833966995", "01775282986","01761115624"];
-        if (!allowedNumbers.includes(formData.username)) {
-            toast.error("Dear Customer,Due to technical issues with our server, our service is still temporarily unavailable. In Sha Allah, we will resolve the issue soon and resume our service. Thank you for your patience.");
-            return;
-        }
+        // const allowedNumbers = ["01739245723", "01680572792", "01833966995", "01775282986","01761115624"];
+        // if (!allowedNumbers.includes(formData.username)) {
+        //     toast.error("Dear Customer,Due to technical issues with our server, our service is still temporarily unavailable. In Sha Allah, we will resolve the issue soon and resume our service. Thank you for your patience.");
+        //     return;
+        // }
         if (!formData.username || !formData.password) {
             setErrorMessage("Please provide required information");
             return;
