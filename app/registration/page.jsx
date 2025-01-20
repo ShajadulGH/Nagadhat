804c1c811e32d40c0bb0b4d1b5e6233e832e7462
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-01-20 12:09:43 +0600

    fix: restore validation for allowed phone numbers in login and registration forms

--- app/registration/page.jsx
+++ app/registration/page.jsx
@@ -147,11 +147,12 @@ const Registration = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // const allowedNumbers = ["01739245723", "01680572792", "01833966995", "01775282986","01761115624"];
-        // if (!allowedNumbers.includes(formData.phone)) {
-        //     toast.error("Dear Customer,Due to technical issues with our server, our service is still temporarily unavailable. In Sha Allah, we will resolve the issue soon and resume our service. Thank you for your patience.");
-        //     return;
-        // }
+        const allowedNumbers = ["01739245723", "01680572792", "01833966995", "01775282986","01761115624"];
+        if (!allowedNumbers.includes(formData.phone)) {
+            toast.error("Dear Customer,Due to technical issues with our server, our service is still temporarily unavailable. In Sha Allah, we will resolve the issue soon and resume our service. Thank you for your patience.");
+            return;
+        }
+        
         async function createUser() {
             const isValidInput = valideateInput(formData);
             if (!isValidInput) {
