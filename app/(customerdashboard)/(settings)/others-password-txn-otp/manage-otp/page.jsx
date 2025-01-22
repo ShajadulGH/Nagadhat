159c6283e05fb269129e1abb3c7982a9f8fa713b
MD Naem <121675549+naem0@users.noreply.github.com>
2025-01-22 12:57:48 +0600

    feat: enhance OTP handling and improve UI layout in affiliate components

--- app/(customerdashboard)/(settings)/others-password-txn-otp/manage-otp/page.jsx
+++ app/(customerdashboard)/(settings)/others-password-txn-otp/manage-otp/page.jsx
@@ -23,7 +23,7 @@ const OTP = () => {
         }
         try {
             const res = await getVerifyOTP({
-                otp_type: otpType,
+                otp_type: otpType == "otp" ? 1 : 2,
                 otp,
                 pin: otpType == "pin" ? pin : ""
             }, session?.accessToken);
