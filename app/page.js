b9bc24557a6e79734284e2b58f2ec59f49fe9273
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-02-10 16:49:06 +0600

    feat: add DownTimeMode component for maintenance notification

--- app/page.js
+++ app/page.js
@@ -4,42 +4,19 @@ import Category from "./components/Category";
 import Sales from "./components/Sales";
 import JustForYou from "./components/JustForYou";
 import Service from "./components/Service";
+import DownTimeMode from "./components/DownTimeMode";
 export default async function Home() {
     return (
         <>
-            {/* <HeroSlider />
+            <HeroSlider />
             <Partner />
             <Sales isHome={true} />
             <Category />
             <JustForYou />
             <div className="pb-5">
                 <Service />
-            </div> */}
-            <div className="d-flex justify-content-center align-items-center vh-100 bg-light px-3">
-                <div
-                    className="bg-white shadow-lg rounded-4 p-5 text-center"
-                    style={{ maxWidth: "600px", width: "100%" }}
-                >
-                    <h1 className="text-danger fw-bold">📢 Dear Users,</h1>
-                    <p className="fs-5 text-secondary mt-3">
-                        Our website will be temporarily unavailable for{" "}
-                        <strong>30 minutes</strong> due to maintenance. We
-                        apologize for any inconvenience and appreciate your
-                        patience.
-                    </p>
-                    <div className="mt-4">
-                        <strong className="d-block fs-5 text-primary">
-                            🔹 Downtime: 30 minutes
-                        </strong>
-                        <strong className="d-block fs-5 text-success">
-                            🔹 We’ll be back shortly!
-                        </strong>
-                    </div>
-                    <p className="fs-6 text-muted mt-4">
-                        Thank you for your understanding. 🙏
-                    </p>
-                </div>
             </div>
+            {/* <DownTimeMode/> */}
         </>
     );
 }
