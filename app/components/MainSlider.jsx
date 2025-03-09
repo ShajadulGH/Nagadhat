317d4a81f3da0b512c57ac3d55b03ad6c2f64e7a
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-03-09 11:28:24 +0600

    feat: add Bootstrap support and remove unused slick carousel styles

--- app/components/MainSlider.jsx
+++ app/components/MainSlider.jsx
@@ -1,7 +1,5 @@
 "use client";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import MainSliderItems from "./MainSliderItems";
 
 const MainSlider = ({ sliderOptionData }) => {
