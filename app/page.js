cbb5b8aaa1fcf7b06c35983e78da1a1af8062961
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-03-23 14:57:03 +0600

    fix: change Home component to async for improved data handling

--- app/page.js
+++ app/page.js
@@ -8,12 +8,11 @@ import Service from "./components/Service";
 import LoginNotice from "./components/home-notice/LoginNotice";
 // import DownTimeMode from "./components/DownTimeMode";
 
-export default function Home() {
+export default async function Home() {
     
     return (
         <>
             <LoginNotice/>
-
             <HeroSlider />
             <Partner />
             <Sales isHome={true} />
