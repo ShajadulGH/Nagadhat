d5c54b2853a963590e796f32bf2f4c2c327bca7a
Shajadul Karim Bhuiyan <shajadulrahe@gmail.com>
2025-04-22 13:24:52 +0600

    redesign-category section

--- app/components/CategoryMainMenu.jsx
+++ app/components/CategoryMainMenu.jsx
@@ -1,7 +1,23 @@
 import CategoryMainMenuList from "./CategoryMainMenuList";
+const iconList = [
+    "/images/dress.png",
+    "/images/giftbox.png",
+    "/images/duffle-bag.png",
+    "/images/cleaning-tools.png",
 
+    "/images/flour.png",
+    "/images/diet.png",
+    "/images/baby-boy.png",
+    "/images/pen.png",
+    "/images/rice-cooker.png",
+    "/images/cosmetology.png",
+];
 const CategoryMainMenu = ({ categoryMenu }) => {
-    const menuItems = categoryMenu;
+    // const menuItems = categoryMenu;
+    const menuItems = categoryMenu.map((item, index) => ({
+        ...item,
+        icon: iconList[index % iconList.length], // loop icons if not enough
+    }));
     return (
         <>
             <ul className="category-menu-area shadow-sm">
--- app/components/CategoryMainMenuList.jsx
+++ app/components/CategoryMainMenuList.jsx
@@ -2,23 +2,23 @@ import Link from "next/link";
 import Image from "next/image";
 import CategorySubMenu from "./CategorySubMenu";
 import { NagadhatPublicUrl } from "../utils";
-
+import { FaChevronRight } from "react-icons/fa";
 const CategoryMainMenuList = ({ menuList }) => {
     const catImage = `${NagadhatPublicUrl}/${menuList.logo}`;
     const categorySubMenuItem = menuList && menuList?.child_categories;
 
     return (
-        <li className="menu-link">
+        <li className="menu-link text-dark ">
             <Link
                 href={`/category/${menuList.slug}`}
                 className="link-item d-flex align-items-center justify-content-between"
             >
-                <p className="d-flex align-items-center">
-                    <span>
+                <p className="d-flex align-items-center category-title ">
+                    <span className="mb-1">
                         <Image
-                            width={23}
-                            height={20}
-                            src={catImage}
+                            width={25}
+                            height={25}
+                            src={menuList.icon}
                             alt={menuList.title ? menuList.title : ""}
                         />
                     </span>
@@ -26,12 +26,13 @@ const CategoryMainMenuList = ({ menuList }) => {
                 </p>
                 {categorySubMenuItem && categorySubMenuItem.length > 0 && (
                     <small>
-                        <Image
+                        {/* <Image
                             width={32}
                             height={32}
                             src="/images/menu-arrow.svg"
                             alt="arrow icon"
-                        />
+                        /> */}
+                        <FaChevronRight size={16} />
                     </small>
                 )}
             </Link>
@@ -44,8 +45,7 @@ const CategoryMainMenuList = ({ menuList }) => {
                         />
                     ))}
                 </ul>
-            )
-            }
+            )}
         </li>
         // )
     );
