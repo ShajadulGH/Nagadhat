77e17758181a420702d85019c5e315fbe365dc86
Shajadul Karim Bhuiyan <shajadulrahe@gmail.com>
2025-04-28 11:16:35 +0600

    feat-2x innerzoom

--- app/components/productDetail/ProductSlider.jsx
+++ app/components/productDetail/ProductSlider.jsx
@@ -40,6 +40,7 @@ function ProductSlider({ productGallery, productInfo }) {
                                     src={`${NagadhatPublicUrl}/${sliderItem.path}`}
                                     zoomSrc={`${NagadhatPublicUrl}/${sliderItem.path}`}
                                     zoomType="hover"
+                                    zoomScale={2} 
                                     zoomPreload={true}
                                     alt="product gallery banner image"
                                     className="img-fluid object-fit-cover"
@@ -66,6 +67,7 @@ function ProductSlider({ productGallery, productInfo }) {
                                 src={image}
                                 zoomSrc={image}
                                 zoomType="hover"
+                                zoomScale={2} 
                                 zoomPreload={true}
                                 alt="product gallery banner image"
                                 className="img-fluid object-fit-cover"
